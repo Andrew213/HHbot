a35c1cebb02261406d2c35d1e9ac4c767e91c20f
Andrey Kochanov <kochanov@lanbilling.ru>
2024-02-05 11:40:34 +0300

    настроил линтеры, подготвил конфиг с добавлением кастомных хостов, установил нужные зависимости

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+    root: true,
+    plugins: [
+        'eslint-plugin',
+        '@typescript-eslint',
+        'import',
+        'eslint-comments'
+    ],
+    env: {
+        es6: true,
+        node: true
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        warnOnUnsupportedTypeScriptVersion: false,
+        project: './tsconfig.json'
+    },
+    settings: {
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.vue']
+        }
+    },
+    extends: [
+        // add more generic rulesets here, such as:
+        'eslint:recommended',
+        'plugin:vue/vue3-recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking'
+    ],
+    rules: {
+        'arrow-parens': ['error', 'as-needed'],
+        quotes: ['error', 'single'],
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            {
+                vars: 'all',
+                args: 'all',
+                varsIgnorePattern: '^_',
+                argsIgnorePattern: '^_',
+                ignoreRestSiblings: true
+            }
+        ],
+        '@typescript-eslint/no-var-requires': 0,
+        '@typescript-eslint/no-unsafe-call': 'off'
+    }
+};
---
+++ scripts/makeAliases.ts
@@ -0,0 +1,41 @@
+/* eslint-disable no-useless-escape */
+import { execSync } from 'child_process';
+import { readFileSync } from 'fs';
+import hosts from '../configs/hosts.json';
+
+const addAliases = (
+    aliases: { host: string; ip: string }[],
+    prefix?: string
+) => {
+    const currentHosts: string = readFileSync('/etc/hosts').toString();
+
+    const newHosts = aliases
+        .reduce(
+            (result, { host, ip }) => {
+                if (currentHosts.includes(host)) {
+                    console.warn(
+                        `warn: host ${host} is already exists in /etc/hosts`
+                    );
+                    return result;
+                }
+                return [...result, `${ip} ${host}`];
+            },
+            [
+                [
+                    `############\n### express-host-aliases\n${
+                        prefix ? `### service: ${prefix}\n` : ''
+                    }`
+                ]
+            ]
+        )
+        .concat('\n############\n');
+
+    if (newHosts.length > 2) {
+        execSync(`sudo sh -c "echo \'${newHosts.join('\n')}\' >> /etc/hosts"`);
+    }
+
+    console.log('newHosts ', newHosts);
+    console.log('currentHosts ', currentHosts);
+};
+
+addAliases(hosts, 'ssr-vue-express');
