0fb33ea0e7f65fa0ab466c761cf0b116ea1c5bbe
Chadwick Czech <kochanov@lanbilling.ru>
2024-03-13 19:59:24 +0300

    update react

--- client/bundles/desktop.tsx
+++ client/bundles/desktop.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { hydrate } from 'react-dom';
+import { hydrateRoot } from 'react-dom/client';
 import { Helmet } from 'react-helmet';
 import { hot } from 'react-hot-loader/root';
 
@@ -25,5 +24,8 @@ const Bundle = props => {
 export const DesktopBundle = hot(Bundle);
 
 export default data => {
-    hydrate(<DesktopBundle data={data} />, document.getElementById('root'));
+    hydrateRoot(
+        document.getElementById('root') as HTMLElement,
+        <DesktopBundle data={data} />
+    );
 };
